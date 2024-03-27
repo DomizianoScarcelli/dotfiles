3842f0245c98eac5c30cc2ee704d843b9b386aac
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2024-03-27 16:08:34 +0100

    sketchybar: added cpu temperature

--- sketchybar/.config/sketchybar/items/widgets/cpu.lua
+++ sketchybar/.config/sketchybar/items/widgets/cpu.lua
@@ -6,6 +6,12 @@ local settings = require("settings")
 -- the cpu load data, which is fired every 2.0 seconds.
 sbar.exec("killall cpu_load >/dev/null; $CONFIG_DIR/helpers/event_providers/cpu_load/bin/cpu_load cpu_update 2.0")
 
+-- To get the SOC temperature, the command is 
+-- /Applications/Stats.app/Contents/Resources/smc list -t | grep "Tp2a" | awk '{print $2}'
+
+--While the average (it seems wrong) is:
+-- /Applications/Stats.app/Contents/Resources/smc list -t | grep "Tp.a" | awk '{sum += $2; count++} END {print sum/count}'
+
 local cpu = sbar.add("graph", "widgets.cpu" , 42, {
   position = "right",
   graph = { color = colors.blue },
@@ -31,8 +37,59 @@ local cpu = sbar.add("graph", "widgets.cpu" , 42, {
   padding_right = settings.paddings + 6
 })
 
+local temp = sbar.add("graph", "widgets.temp" , 42, {
+  position = "right",
+  graph = { color = colors.blue },
+  background = {
+    height = 22,
+    color = { alpha = 0 },
+    border_color = { alpha = 0 },
+    drawing = true,
+  },
+  icon = { string = icons.cpu },
+  label = {
+    string = "temp ??%",
+    font = {
+      family = settings.font.numbers,
+      style = settings.font.style_map["Bold"],
+      size = 9.0,
+    },
+    align = "right",
+    padding_right = 0,
+    width = 5,
+    y_offset = 4
+  },
+  padding_right = 0
+})
+
+
+local function updateTemperature()
+  local temperature = 0.
+  sbar.exec("/Applications/Stats.app/Contents/Resources/smc list -t | grep 'Tp2a' | awk '{print $2}'", function(output)
+    temperature = math.floor(tonumber(output))
+    temp:push({ temperature / 100. })
+    
+    local color = colors.blue
+    if temperature > 30 then
+        if temperature < 60 then
+            color = colors.yellow
+        elseif temperature < 80 then
+            color = colors.orange
+        else
+            color = colors.red
+        end
+    end
+    
+    temp:set({
+        graph = { color = color },
+        label = "temp " .. temperature .. "°",
+    })
+  end)
+end
+
+
 cpu:subscribe("cpu_update", function(env)
-  -- Also available: env.user_load, env.sys_load
+--   -- Also available: env.user_load, env.sys_load
   local load = tonumber(env.total_load)
   cpu:push({ load / 100. })
 
@@ -51,6 +108,7 @@ cpu:subscribe("cpu_update", function(env)
     graph = { color = color },
     label = "cpu " .. env.total_load .. "%",
   })
+  updateTemperature()
 end)
 
 cpu:subscribe("mouse.clicked", function(env)
@@ -58,7 +116,7 @@ cpu:subscribe("mouse.clicked", function(env)
 end)
 
 -- Background around the cpu item
-sbar.add("bracket", "widgets.cpu.bracket", { cpu.name }, {
+sbar.add("bracket", "widgets.cpu.bracket", { cpu.name, temp.name }, {
   background = { color = colors.bg1 }
 })
 
