6a53eb1852b3170906b4c3e8b2ba710a5b09cbf3
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2024-04-18 15:21:01 +0200

    Added oil.nvim

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,9 +1,21 @@
--- function ColorMyPencils(color)
-local color = "catppuccin-mocha"
+local color = 'catppuccin-mocha'
+require("catppuccin").setup(
+{
+    transparent_background=true
+})
+
 vim.cmd.colorscheme(color)
 
-vim.api.nvim_set_hl(0, "Normal", {bg="none"})
-vim.api.nvim_set_hl(0, "NormalFloat", {bg="none"})
--- end
+vim.api.nvim_set_hl(0, 'Normal', {bg='none'})
+vim.api.nvim_set_hl(0, 'NormalFloat', {bg='none'})
+-- vim.api.nvim_set_hl(0, 'TelescopeNormal', {bg='none'})
+-- vim.api.nvim_set_hl(0, 'TelescopeBorder', {bg='none'})
+-- vim.api.nvim_set_hl(0, 'SpecialKey', {bg='none'})
+-- vim.api.nvim_set_hl(0, 'VertSplit', {bg='none'})
+vim.api.nvim_set_hl(0, 'EndOfBuffer', {bg='none'})
+-- vim.api.nvim_set_hl(0, 'EndOfFile', {bg='none'})
+-- vim.api.nvim_set_hl(0, 'SignColumn', {bg='none'})
+-- vim.api.nvim_set_hl(0, 'NonText', {bg='none'})
+-- vim.api.nvim_set_hl(0, 'LineNr', {bg='none'})
+-- vim.api.nvim_set_hl(0, 'Folded', {bg='none'})
 
--- ColorMyPencils()
---
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -0,0 +1,11 @@
+require("oil").setup({
+    columns = {
+        "icon",
+        "size",
+    },
+    delete_to_trash = true,
+    view_options = {
+        show_hidden = true,
+}
+})
+
