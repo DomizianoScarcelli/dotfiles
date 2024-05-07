47ee2471949f792a586277b752dc8b695b791539
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2024-05-07 12:23:23 +0200

    Added firefox configs

---
+++ firefox/Library/Application Support/Firefox/Profiles/w8jic8nd.default-release/user-overrides.js
@@ -0,0 +1,11 @@
+user_pref("privacy.window.maxInnerWidth", 3000);
+user_pref("privacy.window.maxInnerHeight", 2000);
+user_pref("privacy.resistFingerprinting", false); // [FF41+]
+user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
+user_pref("webgl.disabled", true);
+user_pref("privacy.clearOnShutdown.cache", false);     // [DEFAULT: true]
+user_pref("privacy.clearOnShutdown.downloads", true); // [DEFAULT: true]
+user_pref("privacy.clearOnShutdown.formdata", true);  // [DEFAULT: true]
+user_pref("privacy.clearOnShutdown.history", false);   // [DEFAULT: true]
+user_pref("privacy.clearOnShutdown.sessions", false);  // [DEFAULT: true]
+user_pref("security.OCSP.require", false);
